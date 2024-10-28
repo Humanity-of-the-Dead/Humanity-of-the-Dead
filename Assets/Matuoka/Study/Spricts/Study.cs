21727bd0d6623672805040361742b814b8718378
松岡秀亮_プログラマ <matsuoka.s24.vgan@gmail.com>
2024-10-28 21:41:45 +0900

    勉強
    オーディオクリップとPlay関数の勉強

---
+++ Assets/Matuoka/Study/Spricts/Study.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Study : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ////オーディオソース
+        //AudioSource audioSource = GetComponent<AudioSource>();
+
+        ////オーディオソースを流す
+        //audioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
