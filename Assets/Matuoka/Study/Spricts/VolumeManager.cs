dd7ff7a4b762b2d0c59c3c506861e230fa12b0f1
松岡秀亮_プログラマ <matsuoka.s24.vgan@gmail.com>
2024-11-02 19:53:59 +0900

    オーディオミキサーの勉強

--- Assets/Matuoka/Study/Spricts/VolumeManager.cs
+++ Assets/Matuoka/Study/Spricts/VolumeManager.cs
@@ -6,7 +6,7 @@ using TMPro;
 public class VolumeManager : MonoBehaviour
 {
     [SerializeField] AudioSource bgm;
-    TextMeshProUGUI 
+    //TextMeshProUGUI 
 
     // Start is called before the first frame update
     void Start()
