9e0f7dc5a98bfe5a663e044ba6130e87be4c47cb
ukiwacrash <154051150+ukiwacrash@users.noreply.github.com>
2024-11-05 12:57:13 +0900

    TXT

--- Assets/Kagawa/script/PlayerMoveAnimation.cs
+++ Assets/Kagawa/script/PlayerMoveAnimation.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 
 public class PlayerMoveAnimation : MonoBehaviour
 {
-    [Header("全身")]public GameObject playerRc;
-    [SerializeField, Header("腕、先に右手")] public GameObject[] arm;     
-    [SerializeField, Header("太腿、先に右足")] public GameObject[] leg;   
-    [SerializeField, Header("すね、先に右足")] public GameObject[] foot;
+    [SerializeField, Header("頭のImage")] SpriteRenderer headSR;
+    [SerializeField, Header("腕のImage、先に右手")] SpriteRenderer armSR;
+    [SerializeField, Header("足のImage、先に右足")] SpriteRenderer legSR;
+
+    [Header("全身")] public GameObject playerRc;
+    [SerializeField, Header("腕の角度、先に右手")] GameObject[] arm;
+    [SerializeField, Header("太腿の角度、先に右足")] GameObject[] leg;
+    [SerializeField, Header("すねの角度、先に右足")] GameObject[] foot;
 
     [Header("全身の角度")] public float[] playerRotation;
     [Header("腕の角度")] public float[] armRotation;
@@ -32,7 +36,7 @@ public class PlayerMoveAnimation : MonoBehaviour
     // 向いている方向が右を向いているか
     bool isMirror;
 
-    // 継続して歩くフラグ
+    // 方向フラグ(右 = false)
     bool isWalk;
 
     // タイマー
@@ -53,48 +57,48 @@ public class PlayerMoveAnimation : MonoBehaviour
     void Update()
     {
         time -= Time.deltaTime;
+
         if (Input.GetKeyDown(KeyCode.D))
         {
-            // プレイヤーの向きが左から右に変わったとき
-            if (!isMirror)
-            {
-                shaft = 0;
-                MoveMirror();
-            }
-        }
-
-        if(Input.GetKeyDown(KeyCode.A))
-        {
-            // プレイヤーの向きが右から左に変わったとき
-            if (isMirror)
-            {
-                shaft = 180;
-                MoveMirror();
-            }
-        }
-
-
-        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A))
-        {
-           
-            // 連続入力されているか
-            if (time - 0.05 < 0)
-            {
-                isWalk = true;
-                //isActive = true;
-            }
-
             // 歩く動作をしている時、呼ばせない
             if (time < 0)
             {
-                if (isWalk)
-                {
-                    // 配列の中の値をマイナスにする
-                    KeepWalk();
-                    isWalk = false;
-                }
-                time = timeMax * armRotation.Length;
-                StartCoroutine(CallFunctionWithDelay());
+                // プレイヤーの向きが左から右に変わったとき
+                isWalk = false;
+                shaft = 0;
+
+                isActive = false;
+                ChangeArmAnime();
+                WalkStart();
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            // 歩く動作をしている時、呼ばせない
+            if (time < 0)
+            {
+                // プレイヤーの向きが右から左に変わったとき
+                isWalk = true;
+                shaft = 180;
+
+                isActive = false;
+                ChangeArmAnime();
+                WalkStart();
+            }
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            if (!isWalk)
+            {
+                KeepWalk();
+            }
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            if (isWalk)
+            {
+                KeepWalk();
             }
         }
     }
@@ -110,17 +114,24 @@ public class PlayerMoveAnimation : MonoBehaviour
         // 腕のアニメーション
         if (arm == null || armRotation == null)
         {
+            Debug.LogWarning("armのデータが何かしら抜けてる");
             return;
         }
         else
         {
-            arm[0].transform.rotation = Quaternion.Euler(0, 0, armRotation[indexNumber]);
-            arm[1].transform.rotation = Quaternion.Euler(0, 180, armRotation[indexNumber]);
+            arm[0].transform.rotation = Quaternion.Euler(0, shaft, armRotation[indexNumber]);
+            arm[1].transform.rotation = Quaternion.Euler(0, shaft + 180, armRotation[indexNumber]);
         }
 
         // 足のアニメーション
-        if (leg == null ||foot == null)
+        if (leg == null || legBackRotation == null || legForwardRotation == null)
         {
+            Debug.LogWarning("Legのデータが何かしら抜けてる");
+            return;
+        }
+        else if (foot == null || footBackRotation == null || footForwardRotation == null)
+        {
+            Debug.LogWarning("footのデータが何かしら抜けてる");
             return;
         }
         else
@@ -128,23 +139,23 @@ public class PlayerMoveAnimation : MonoBehaviour
             // 歩き始めの場合
             if (!isActive)
             {
-                leg[0].transform.rotation = Quaternion.Euler(0, 0, legBackRotation[indexNumber]);
-                leg[1].transform.rotation = Quaternion.Euler(0, 0, legForwardRotation[indexNumber]);
-                foot[0].transform.rotation = Quaternion.Euler(0, 0,  footBackRotation[indexNumber]);
-                foot[1].transform.rotation = Quaternion.Euler(0, 0,  footForwardRotation[indexNumber]);
+                leg[0].transform.rotation = Quaternion.Euler(0, shaft, legBackRotation[indexNumber]);
+                leg[1].transform.rotation = Quaternion.Euler(0, shaft, legForwardRotation[indexNumber]);
+                foot[0].transform.rotation = Quaternion.Euler(0, shaft, footBackRotation[indexNumber]);
+                foot[1].transform.rotation = Quaternion.Euler(0, shaft, footForwardRotation[indexNumber]);
             }
             //歩き続けている場合
             if (isActive)
             {
-                leg[0].transform.rotation = Quaternion.Euler(0, 0, legForwardRotation[indexNumber]);
-                leg[1].transform.rotation = Quaternion.Euler(0, 0, legBackRotation[indexNumber]);
-                foot[0].transform.rotation = Quaternion.Euler(0, 0, footForwardRotation[indexNumber]);
-                foot[1].transform.rotation = Quaternion.Euler(0, 0, footBackRotation[indexNumber]);
+                leg[0].transform.rotation = Quaternion.Euler(0, shaft, legForwardRotation[indexNumber]);
+                leg[1].transform.rotation = Quaternion.Euler(0, shaft, legBackRotation[indexNumber]);
+                foot[0].transform.rotation = Quaternion.Euler(0, shaft, footForwardRotation[indexNumber]);
+                foot[1].transform.rotation = Quaternion.Euler(0, shaft, footBackRotation[indexNumber]);
             }
-        }    
+        }
     }
 
-    private IEnumerator CallFunctionWithDelay()
+    IEnumerator CallFunctionWithDelay()
     {
         for (int i = 0; i < armRotation.Length; i++)
         {
@@ -152,38 +163,75 @@ public class PlayerMoveAnimation : MonoBehaviour
 
             // indexNumberの値を増やす(配列番号を上げる)
             indexNumber = (indexNumber + 1) % armRotation.Length;
-
-            // 配列の中の値を元に戻す
-            if(isActive)
-            {
-                KeepWalk();
-                isActive = false;
-            }
-            yield return new WaitForSeconds(timeMax); 
+            yield return new WaitForSeconds(timeMax);
         }
     }
 
-    
+
     /// <summary>
-    /// 歩くことを継続した時
-    /// 配列の中の値を逆にする
+    /// 歩くことを継続した時、腕の配列の中の値を逆にする
+    /// </summary>
+    void ChangeArmAnime()
+    {
+        //三項演算子(各要素に対して変換操作を行う)
+        if (isActive)
+        {
+            armRotation = armRotation.Select(value => value > 0 ? -value : value).ToArray();
+        }
+        else if (!isActive)
+        {
+            armRotation = armRotation.Select(value => value < 0 ? -value : value).ToArray();
+        }
+    }
+
+    /// <summary>
+    /// 歩き始めの関数
+    /// </summary>
+    void WalkStart()
+    {
+        time = timeMax * armRotation.Length;
+        StartCoroutine(CallFunctionWithDelay());
+    }
+
+    /// <summary>
+    /// 歩くことを継続したとき
     /// </summary>
     void KeepWalk()
     {
-
-        armRotation = armRotation.Select(value => value > 0 ? -value : value).ToArray();
+        // 連続入力されているか
+        if (time - 0.05 < 0)
+        {
+            isActive = !isActive;
+            ChangeArmAnime();
+            WalkStart();
+        }
     }
 
     /// <summary>
-    /// 向く方向が変わったとき配列の中の値を逆にする
+    /// 頭のイメージ
     /// </summary>
-    void MoveMirror()
+    /// <param name="head">画像データ</param>
+    public void ChangeHead(BodyPartsData head)
     {
-        armRotation = armRotation.Select(value => value > 0 ? -value : value).ToArray();
-        legForwardRotation = armRotation.Select(value => value > 0 ? -value : value).ToArray();
-        legBackRotation = armRotation.Select(value => value > 0 ? -value : value).ToArray();
-        footForwardRotation = armRotation.Select(value => value > 0 ? -value : value).ToArray();
-        footBackRotation = armRotation.Select(value => value > 0 ? -value : value).ToArray();
+
+        headSR.sprite = head.sPartSprite;
+    }
+
+    /// <summary>
+    /// 腕のイメージ
+    /// </summary>
+    /// <param name="arm">画像データ</param>
+    public void ChangeArm(BodyPartsData arm)
+    {
+        armSR.sprite = arm.sPartSprite;
+    }
+
+    /// <summary>
+    /// 足のイメージ
+    /// </summary>
+    /// <param name="leg">画像データ</param>
+    public void ChangeLeg(BodyPartsData leg)
+    {
+        legSR.sprite = leg.sPartSprite;
     }
 }
-
