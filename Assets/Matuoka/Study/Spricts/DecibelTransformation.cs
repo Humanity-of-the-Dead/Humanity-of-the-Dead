a177fec92df744589ae7784417637b4cdf09ac88
松岡秀亮_プログラマ <matsuoka.s24.vgan@gmail.com>
2024-11-05 21:00:44 +0900

    デシベル変換の勉強

--- Assets/Matuoka/Study/Spricts/DecibelTransformation.cs
+++ Assets/Matuoka/Study/Spricts/DecibelTransformation.cs
@@ -1,29 +1,46 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DecibelTransformation : MonoBehaviour
 {
-    ////オーディオミキサー
-    //[SerializeField] AudioMixer audMix;
-
     //ボリューム0~1
-    [SerializeField]float vol;
+    [Header("ボリューム0~1")][SerializeField,Range(0f,1f)]float vol;
     //デシベル(ボリューム-80~0)
-    float dec;
+    [Header("デシベル")][SerializeField,Range(-80f,0f)]float dec;
+
+    //ボリュームからデシベルに変換された値
+    float volToDec_Dec;
+    //デシベルからボリュームに変換された値
+    float decToVol_Vol;
 
     // Start is called before the first frame update
     void Start()
     {
-        //ボリュームをデシベルに変換
-        dec = Mathf.Clamp(Mathf.Log10(vol) * 20f,-80f,0f);
-
-        Debug.Log(dec);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //ボリュームをデシベルに変換
+        VolumeToDecibelTransformation();
+        //デシベルをボリュームに変換
+        DecibelToVolumeTransformation();
+
+        Debug.Log("デシベル:" + volToDec_Dec + "\nボリューム:" + decToVol_Vol);
+    }
+
+    //ボリュームからデシベルに変換
+    void VolumeToDecibelTransformation()
+    {
+        //ボリュームをデシベルに変換
+        volToDec_Dec = Mathf.Clamp(Mathf.Log10(vol) * 20f, -80f, 0f);
+    }
+
+    //デシベルからボリュームに変換
+    void DecibelToVolumeTransformation()
+    {
+        decToVol_Vol = Mathf.Clamp(Mathf.Pow(10f, dec / 20f), 0f, 1f);
     }
 }
