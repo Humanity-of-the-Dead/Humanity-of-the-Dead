6bf7a3cdea5baee1565477bf4e07ca5d8f3739f3
maho0628 <maho06282001@gmail.com>
2024-11-07 17:10:21 +0900

    シーン遷移汎用的なもの完成

---
+++ Assets/Yamashina/Script/ScriptableObject/SceneInformation.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "SceneInformation", menuName = "ScriptableObjects/StageInformation")]
+
+public class SceneInformation : ScriptableObject
+{
+    // 全てのシーン
+    // 全てのシーン
+    [System.Serializable]
+    public enum SCENE
+    {
+        Title,      // タイトル
+        StageOne,   // ステージ１
+        StageTwo,   // ステージ２
+        StageThree, // ステージ３
+
+    }
+    // ステージのシーン
+    [System.Serializable]
+    public enum STAGE
+    {
+        One,      // ステージ１
+        Two,      // ステージ２
+        Three,    // ステージ３
+    }
+
+    [SerializeField] public SceneObject[] sceneObject;
+    [SerializeField] public string[] sceneNames;// シーンの名前
+    [SerializeField] private SCENE previousScene;
+
+    public SceneObject GetSceneObject(SCENE scene)
+    {
+        return sceneObject[(int)scene];
+    }
+
+    public string GetSceneName(SCENE scene)
+    {
+        return sceneNames[(int)scene];
+    }
+
+    public SCENE GetPreviousScene() { return previousScene; }
+
+    public void SetPreviousScene(SCENE scene) { previousScene = scene; }
+}
+
+
