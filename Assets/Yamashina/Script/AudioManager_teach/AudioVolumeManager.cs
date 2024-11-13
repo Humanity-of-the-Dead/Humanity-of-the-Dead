22fe1ed71226684c413591ba30839513aa66bf4b
maho0628 <maho06282001@gmail.com>
2024-11-13 14:08:02 +0900

    コミット

--- Assets/Yamashina/Script/AudioManager_teach/AudioVolumeManager.cs
+++ Assets/Yamashina/Script/AudioManager_teach/AudioVolumeManager.cs
@@ -6,48 +6,47 @@ using UnityEngine.Audio;
 
 public class AudioVolumeManager : MonoBehaviour
 {
-    public AudioMixer audioMixer; // MainAudioMixerを割り当て
+
+    public AudioMixer audioMixer; // Reference to the main AudioMixer
     public Slider bgmSlider;
     public Slider seSlider;
     public Slider uiSlider;
 
+    // Keys for saving volume preferences
+    private const string BGM_PREF_KEY = "BGM_Volume";
+    private const string SE_PREF_KEY = "SE_Volume";
+    private const string UI_PREF_KEY = "UI_Volume";
+
     private void Start()
     {
-        // スライダーの初期値をPlayerPrefsから取得またはデフォルト1.0fで設定
-        bgmSlider.value = PlayerPrefs.GetFloat("BGM_Volume", 1.0f);
-        seSlider.value = PlayerPrefs.GetFloat("SE_Volume", 1.0f);
-        uiSlider.value = PlayerPrefs.GetFloat("UI_Volume", 1.0f);
+        // Set slider values from saved preferences or default to 1.0f
+        bgmSlider.value = PlayerPrefs.GetFloat(BGM_PREF_KEY, 1.0f);
+        seSlider.value = PlayerPrefs.GetFloat(SE_PREF_KEY, 1.0f);
+        uiSlider.value = PlayerPrefs.GetFloat(UI_PREF_KEY, 1.0f);
 
-        // スライダー変更時に音量を更新し、保存
-        bgmSlider.onValueChanged.AddListener(value => SetVolume("BGM_Volume", value));
-        seSlider.onValueChanged.AddListener(value => SetVolume("SE_Volume", value));
-        uiSlider.onValueChanged.AddListener(value => SetVolume("UI_Volume", value));
+        // Set up slider listeners to update and save volume changes
+        bgmSlider.onValueChanged.AddListener(value => SetVolume(BGM_PREF_KEY, "BGM_Volume", value));
+        seSlider.onValueChanged.AddListener(value => SetVolume(SE_PREF_KEY, "SE_Volume", value));
+        uiSlider.onValueChanged.AddListener(value => SetVolume(UI_PREF_KEY, "UI_Volume", value));
     }
 
-    // ボリュームをdBに変換してミキサーに設定
-    private void SetVolume(string exposedParam, float volume)
+    // Method to set volume and save to PlayerPrefs
+    private void SetVolume(string prefKey, string exposedParam, float volume)
     {
         float dbVolume = Mathf.Log10(Mathf.Clamp(volume, 0.0001f, 1f)) * 20f;
         audioMixer.SetFloat(exposedParam, dbVolume);
-
-        // ボリューム設定を保存
-        PlayerPrefs.SetFloat(exposedParam, volume);
-    }
-    private void Update()
-    {
-        Debug.Log(uiSlider.value);
-        Debug.Log(seSlider.value);
-
-
+        PlayerPrefs.SetFloat(prefKey, volume);
+        PlayerPrefs.Save();
     }
 
     private void OnDestroy()
     {
-        // スライダーのリスナー解除
-        bgmSlider.onValueChanged.RemoveListener(value => SetVolume("BGM_Volume", value));
-        seSlider.onValueChanged.RemoveListener(value => SetVolume("SE_Volume", value));
-        uiSlider.onValueChanged.RemoveListener(value => SetVolume("UI_Volume", value));
+        // Remove listeners to prevent memory leaks
+        bgmSlider.onValueChanged.RemoveListener(value => SetVolume(BGM_PREF_KEY, "BGM_Volume", value));
+        seSlider.onValueChanged.RemoveListener(value => SetVolume(SE_PREF_KEY, "SE_Volume", value));
+        uiSlider.onValueChanged.RemoveListener(value => SetVolume(UI_PREF_KEY, "UI_Volume", value));
     }
 }
+
 
 
