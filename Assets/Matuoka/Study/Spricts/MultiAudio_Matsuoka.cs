14f2ffc437b56289db1bf172c168c7c493d944e0
松岡秀亮_プログラマ <matsuoka.s24.vgan@gmail.com>
2024-11-14 20:01:43 +0900

    複数の音源を流せるようにした
    UIの判定を行頭がUIかどうかにした

---
+++ Assets/Matuoka/Study/Spricts/MultiAudio_Matsuoka.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MultiAudio_Matsuoka: MonoBehaviour
+{
+    //BGMのオーディオクリップ
+    [SerializeField] AudioClip[] bGMAudCli;
+    //SEのオーディオクリップ
+    [SerializeField] AudioClip[] sEAudCli;
+
+    //BGM用のオーディオソース
+    AudioSource bGMAudSou;
+    //SE用のオーディオソース
+    AudioSource sEAudSou;
+
+    //BGM,SE,UIのオーディオミキサーグループ
+    AudioMixerGroup bGMAudMixGro, sEAudMixGro, uIAudMixGro;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //BGMのオーディオソースを取得
+        bGMAudSou=GameObject.FindWithTag("BGM").GetComponent<AudioSource>();
+        //SEのオーディオソース取得
+        sEAudSou=GameObject.FindWithTag("SE").GetComponent<AudioSource>();
+
+        //AudioSourceにオーディオミキサーグループを割り当てる
+        if(bGMAudSou != null )bGMAudSou.outputAudioMixerGroup = bGMAudMixGro;
+        if (sEAudSou != null) sEAudSou.outputAudioMixerGroup = sEAudMixGro;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //BGMを選んで再生
+    public void ChooseSongsBGM(int ind) {
+        //indがbGMAudCliの要素番号のとき
+        if (ind >= 0 && ind < bGMAudCli.Length)
+        {
+            bGMAudSou.clip = bGMAudCli[ind];
+
+            if (bGMAudSou != null)
+            {
+                bGMAudSou.Play();
+                Debug.Log("Playing BGM:" + bGMAudSou.clip.name);
+            }
+            else
+            {
+                //警告メッセージ
+                Debug.LogWarning("BGM clip not set");
+            }
+        }
+        else
+        {
+            Debug.LogWarning("BGM index out of range");
+        }
+    }
+
+    //SEを選んで再生
+    public void ChooseSongsSE(int ind)
+    {
+        //indがsEAudCliの要素番号のとき
+        if (ind >= 0 && ind < sEAudCli.Length)
+        {
+            sEAudSou.clip = sEAudCli[ind];
+
+            if (sEAudSou != null)
+            {
+                //clip名にの行頭がUIのとき
+                if (sEAudCli[ind].name.StartsWith("UI"))
+                {
+                    sEAudSou.outputAudioMixerGroup = uIAudMixGro;
+                    Debug.Log("行頭がUI");
+                }
+                else
+                {
+                    sEAudSou.outputAudioMixerGroup= sEAudMixGro;
+                }
+
+                sEAudSou.PlayOneShot(sEAudSou.clip);
+                Debug.Log("Playing SE:" + sEAudSou.clip.name);
+            }
+            else
+            {
+                //警告メッセージ
+                Debug.LogWarning("SE clip not set");
+            }
+        }
+        else
+        {
+            Debug.LogWarning("SE index out of range");
+        }
+    }
+}
