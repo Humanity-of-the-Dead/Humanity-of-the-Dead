fab6603c5d0b4e07597bd68156fa267c7ceeceaf
Sakamoto0722 <teruteru.0722.s@gmail.com>
2024-11-18 00:47:31 +0900

    11/17作業分

--- Assets/Sakamoto/Scripts/EnemySpooner.cs
+++ Assets/Sakamoto/Scripts/EnemySpooner.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemySpooner : MonoBehaviour
@@ -9,6 +10,8 @@ public class EnemySpooner : MonoBehaviour
     //プレイヤーパラメーター
     [SerializeField] GameObject goPlayerParameter;
     [SerializeField] PlayerParameter playerParameter;
+    //プレイヤーコントローラ
+    [SerializeField] GameObject goPlayerControl;
     //ゲームマネージャー
     [SerializeField] GameMgr gameMgr;
     //プレイヤーオブジェクト
@@ -18,12 +21,25 @@ public class EnemySpooner : MonoBehaviour
 
     //タイマー
     float fTimer;
+    //タイマーの最大値
+    [SerializeField] float fTimerMax;
+
+    [SerializeField] float fEnemyMax;
+
+    private void Start()
+    {
+        //エネミーを上限-1体生み出す
+        for(int i = 0; i < fEnemyMax - 1; i++)
+        {
+            createEnemy();
+        }
+    }
 
     void Update()
     {
         if(Vector2.Distance(this.transform.position,goTarget.transform.position) < 20)
         {
-            if(fTimer > 3) 
+            if (fTimer > fTimerMax && liEnemyList.Count < fEnemyMax) 
             {
                 createEnemy();
                 fTimer = 0;
@@ -33,6 +49,14 @@ public class EnemySpooner : MonoBehaviour
         else
         {
             fTimer = 0;
+        }
+
+        for(int i = 0; i < liEnemyList.Count; i++)
+        {
+            if(liEnemyList[i] == null)
+            {
+                liEnemyList.Remove(liEnemyList[i]);
+            } 
         }
 
         //if (Input.GetKeyDown(KeyCode.Space))
@@ -47,7 +71,9 @@ public class EnemySpooner : MonoBehaviour
         //敵のインスタンスを生成
         liEnemyList.Add(Instantiate(goEnemyObject));
         //プレイヤーパラメーターを渡す
-        liEnemyList[liEnemyList.Count-1].GetComponent<EnemyParameters>().PlayerParameter = goPlayerParameter;
+        liEnemyList[liEnemyList.Count　-　1].GetComponent<newEnemyParameters>().PlayerParameter = goPlayerParameter;
+        //プレイヤーコントローラを渡す
+        liEnemyList[liEnemyList.Count　-　1].GetComponent<newEnemyParameters>().PlayerControl = goPlayerControl;
         liEnemyList[liEnemyList.Count - 1].GetComponent<newEnemyMovement>().scPlayerParameter = playerParameter;
         //ゲームマネージャーを渡す
         liEnemyList[liEnemyList.Count - 1].GetComponent<newEnemyMovement>().gamestate = gameMgr;
