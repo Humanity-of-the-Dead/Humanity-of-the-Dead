c5c81c027511031b51a7171542becd80b8c4c481
松岡秀亮_プログラマ <matsuoka.s24.vgan@gmail.com>
2024-11-26 22:17:16 +0900

    作業途中

---
+++ Assets/Matuoka/Spricts/OptionButton.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionButton : MonoBehaviour
+{
+    //オプション画面
+    [SerializeField] GameObject OptScr;
+
+    //オプション画面を表示しているか
+    bool isShoOptScr = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //オプションボタンが押されたとき
+    //オプション画面を表示
+    public void OnCrickOptionButton()
+    {
+
+    }
+}
