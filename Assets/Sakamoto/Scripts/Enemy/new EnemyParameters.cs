7d4f87a9312dc651f21887460aff0023a302622b
maho0628 <maho06282001@gmail.com>
2025-01-10 17:54:22 +0900

    敵に攻撃を当てた時(部位を破壊した時?)のSEが異様に大きく音割れする問題解決

--- Assets/Sakamoto/Scripts/Enemy/new EnemyParameters.cs
+++ Assets/Sakamoto/Scripts/Enemy/new EnemyParameters.cs
@@ -172,6 +172,8 @@ public class newEnemyParameters : MonoBehaviour
             UpperHP -= damage;
             ShowHitEffects(body);
             UpdateHPBar(UpperHPBar, UpperHP, MaxUpperHP);
+            MultiAudio.ins.PlaySEByName("SE_common_hit_attack");
+
             Debug.Log(UpperHP);
             Debug.Log(MaxUpperHP);
 
@@ -183,6 +185,8 @@ public class newEnemyParameters : MonoBehaviour
             LowerHP -= damage;
             ShowHitEffects(body);
             UpdateHPBar(LowerHPBar, LowerHP, MaxLowerHP);
+            MultiAudio.ins.PlaySEByName("SE_common_hit_attack");
+
             Debug.Log(LowerHP);
             Debug.Log(MaxLowerHP);
         }
