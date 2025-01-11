4aebffa150341a26958240cf8d20e6774a090577
maho0628 <maho06282001@gmail.com>
2025-01-11 16:09:56 +0900

    リファクタリングと修正

--- Assets/Sakamoto/Scripts/GameMgr.cs
+++ Assets/Sakamoto/Scripts/GameMgr.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public enum GameState { 
+public enum GameState
+{
     Main,
     ShowOption,
     ShowText,
@@ -29,58 +30,67 @@ public class GameMgr : MonoBehaviour
 
     private void Update()
     {
-        if (enGameState == GameState.BeforeBoss)
-        {
-            ForceEnemiesMoveLeft();  // 雑魚キャラを左移動させる
-            ChangeState(GameState.AfterBOss); // 移動後に AfterBoss に切り替える
-        }
-        if (enGameState == GameState.Main && Input.GetKeyDown(KeyCode.G))
-        {
-            OptionButton.onClick.Invoke();
-            //enGameState = GameState.ShowText;
-        }
-        else if (enGameState == GameState.ShowOption && Input.GetKeyDown(KeyCode.G))
-        {
-            OptionReturnButton.onClick.Invoke();
-            //enGameState = GameState.Main;
-        }
 
-        if (enGameState == GameState.GameOver)
+        switch (GetState())
         {
-            if (timer > 1)
+            case GameState.BeforeBoss:
+                ForceEnemiesMoveLeft();  // 雑魚キャラを左移動させる
+                ChangeState(GameState.AfterBOss); // 移動後に AfterBoss に切り替える
+                break;
+            case GameState.Main:
+                if (Input.GetKeyDown(KeyCode.G))
+                {
+                    OptionButton.onClick.Invoke();
+
+                }
+                break;
+            case GameState.ShowOption:
+                if (Input.GetKeyDown(KeyCode.G))
+                {
+                    OptionReturnButton.onClick.Invoke();
+
+                }
+
+                break;
+            case GameState.GameOver:
+
+                if (timer > 1)
+                {
+                    SceneTransitionManager.instance.ReloadCurrentScene();
+                    timer = 0;
+                }
+                timer += Time.deltaTime;
+
+
+                break;
+        }
+    }
+
+        //ステートチェンジ
+        public static void ChangeState(GameState newState)
+        {
+            enGameState = newState;
+        }
+        public static GameState GetState()
+        {
+            return enGameState;
+        }
+        private void ForceEnemiesMoveLeft()
+        {
+            GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+            foreach (GameObject enemy in enemies)
             {
-                SceneTransitionManager.instance.ReloadCurrentScene();
-                timer = 0;
+                enemy.transform.Translate(Vector3.left * 5f); // 任意の速度で左移動
+                EnemyMoveAnimation moveAnimation = GameObject.FindAnyObjectByType<EnemyMoveAnimation>();
+                moveAnimation.LeftMove();
             }
-            timer += Time.deltaTime;
-        }
-    }
 
-    //ステートチェンジ
-    public static void ChangeState(GameState newState)
-    {
-        enGameState = newState;
-    }
-    public static GameState GetState()
-    {
-        return enGameState;
-    }
-    private void ForceEnemiesMoveLeft()
-    {
-        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
-        foreach (GameObject enemy in enemies)
+        }
+        private void OnGUI()
         {
-            enemy.transform.Translate(Vector3.left * 5f); // 任意の速度で左移動
-            EnemyMoveAnimation  moveAnimation =GameObject.FindAnyObjectByType<EnemyMoveAnimation>();
-            moveAnimation.LeftMove();
+            GUI.skin.label.fontSize = 30;  // 例えば30に設定
+
+            GUI.Label(new Rect(10.0f, 10.0f, Screen.width, Screen.height), enGameState.ToString());
         }
-
     }
-    private void OnGUI()
-    {
-        GUI.skin.label.fontSize = 30;  // 例えば30に設定
-
-        GUI.Label(new Rect(10.0f, 10.0f, Screen.width, Screen.height), enGameState.ToString());
-    }
-}
 
