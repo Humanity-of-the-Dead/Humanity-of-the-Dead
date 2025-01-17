024b3121a29b5dc25dc82a9bf0dc4f0f5e1c27c9
maho0628 <maho06282001@gmail.com>
2025-01-17 20:24:26 +0900

    コミット

--- Assets/Sakamoto/Scripts/Player/PlayerControl.cs
+++ Assets/Sakamoto/Scripts/Player/PlayerControl.cs
@@ -51,6 +51,8 @@ public class PlayerControl : MonoBehaviour
     //ターゲット
     [SerializeField, Header("ボスのオブジェクトを入れる\nボス以外はスポナーが勝手に生成")]
     private List<GameObject> enemyObject;
+    private float originalGravityScale;
+
     //[SerializeField] GameObject[] goObj;
 
 
@@ -146,273 +148,284 @@ public class PlayerControl : MonoBehaviour
 
             if (Input.GetKey(KeyCode.W) && jumpCount < 1)
             {
-                Rigidbody2D playerRigidBody2D=GetComponent<Rigidbody2D>();
-                playerRigidBody2D.AddForce(transform.up * playerJumpPower);
-                MultiAudio.ins.PlaySEByName("SE_hero_action_jump");
-                isJump = true;
-                jumpCount++;
-                if (playerRigidBody2D.velocity.y > playerJumpPower / playerRigidBody2D.mass)
-                {
-                    playerRigidBody2D.velocity = new Vector2(playerRigidBody2D.velocity.x, playerJumpPower / playerRigidBody2D.mass);
-                }
-            }
-        }
-    }
-    private void UpdateTimers()
-    {
-        playerMoveAnimation.timeWalk -= Time.deltaTime;
-        playerMoveAnimation.timeAttack -= Time.deltaTime;
-    }
-    //ゲームメインのエクスキュート
-    void MainExecution()
-    {
-        //現在のポジションを取得
-        Vector3 vPosition = transform.position;
 
-        //カメラとの距離の絶対値が一定以下ならプレイヤーが動く　画面外に出ないための処置
-        //移動
-        Vector3 vPosFromCame = vPosition - mainCamera.transform.position; //カメラ基準のプレイヤーの位置
-
-        if (!playerMoveAnimation.SetAttack())
-        {
-            //左移動
-            if (Input.GetKey(KeyCode.A))
-            {
-                if (vPosFromCame.x > -mainCameraWidth / 2)
-                {
-                    vPosition.x -= Time.deltaTime * playerSpeed;
-
-                }
-                playerMoveAnimation.HandleWalk(180);
-            }
-            //右移動
-            if (Input.GetKey(KeyCode.D))
-            {
-                if (mainCameraWidth / 2 > vPosFromCame.x)
-                {
-                    vPosition.x += Time.deltaTime * playerSpeed;
-                }
-                playerMoveAnimation.HandleWalk(0);
-
-            }
-
-            //ジャンプ
-
-            if (Input.GetKey(KeyCode.W) && jumpCount < 1)
-            {
-                GetComponent<Rigidbody2D>().AddForce(transform.up * playerJumpPower);
+                Rigidbody2D playerRigidBody2D = GetComponent<Rigidbody2D>();
+                playerRigidBody2D.gravityScale = 3.0f;  // ジャンプ中の重力スケール
+                playerRigidBody2D.velocity = new Vector2(rb.velocity.x, playerJumpPower);
                 MultiAudio.ins.PlaySEByName("SE_hero_action_jump");
                 isJump = true;
                 jumpCount++;
             }
-
-            //楽に次のシーン行きたいならこの下のコードをコメントアウト解除　確認後コメントアウトしておいて
-
-            //if (Input.GetKeyDown(KeyCode.Escape))
-            //{
-            //    SceneTransitionManager.instance.NextSceneButton(SceneTransitionManager.instance.sceneInformation.GetCurrentScene() + 1); 
-            //}
-            //ここまで
-            //楽にボス戦行きたいなら以下のコードをコメント解除
-            //if (Input.GetKeyDown(KeyCode.S))
-            //{
-            //    vPosition = new Vector2(190.0f, -1.536416f);
-            //}
-            //ここまで
-
-        }
-
-        //体が回転しないようにするのオイラーを０で設定すればできる
-        //自分のtransformを取得
-
-        transform.rotation = Quaternion.Euler(0f, 0f, 0f);
-        transform.position = vPosition;
-
-
-
-
-        #region 山品変更
-        //アニメーションをここで呼ぶため、追記
-        //攻撃関連
-        if (!playerMoveAnimation.SetAttack() && playerMoveAnimation.timeAttack < 0)
-        {
-            //上半身攻撃
-            if (Input.GetKeyDown(KeyCode.I))
+            else if (rb.velocity.y <= 0)  // ジャンプ終了時に重力スケールを元に戻す
             {
-
-                playerMoveAnimation.PantieStart();
-                #endregion
-                switch (PlayerParameter.Instance.UpperData.upperAttack)
-                {
-                    case UpperAttack.NORMAL:
-
-                        MultiAudio.ins.PlaySEByName("SE_hero_attack_upper");
-
-                        break;
-
-                    case UpperAttack.POLICE:
-                        Vector2 ShootMoveBector = new Vector2(0, 0);
-                        //子のplayerRCのローテーションYを持ってくる
-                        // y = 0のときは右向き、0 y = 180のときは左向き
-                        Debug.Log(transform.GetChild(0).transform.eulerAngles.y);
-                        if (transform.GetChild(0).transform.eulerAngles.y == 180)
-                        {
-                            ShootMoveBector.x = -1;
-                        }
-                        else
-                        {
-                            ShootMoveBector.x = 1;
-                        }
-
-                        Debug.Log(ShootMoveBector);
-                        Debug.Log("shootFlagは" + isShot);
-
-                        //isShotがtrueなら銃を発射する
-                        if (isShot == true)
-                        {
-                            Debug.Log("弾発射");
-                            Gun.Shoot(ShootMoveBector, transform);
-
-                            MultiAudio.ins.PlaySEByName("SE_policeofficer_attack_upper");
-
-                            //bShootFlag = false;
-                        }
-                        break;
-
-                    case UpperAttack.NURSE:
-                        MultiAudio.ins.PlaySEByName("SE_nurse_attack_upper");
-                        break;
-                }
-
-                if (PlayerParameter.Instance.UpperData.sPartsName == "ボスの上半身")
-                {
-                    MultiAudio.ins.PlaySEByName("SE_lastboss_attack_upper");
-                }
-
-                for (int i = 0; i < enemyObject.Count; i++)
-                {
-                    //Debug.Log(liObj[i].gameObject.transform.position);
-                    //Debug.Log(playerParameter.UpperData.AttackArea);
-                    //仮引数
-                    UpperBodyAttack(i, enemyObject[i].transform.position, PlayerParameter.Instance.UpperData.AttackArea, PlayerParameter.Instance.UpperData.iPartAttack);
-                }
+                rb.gravityScale = originalGravityScale;
             }
-            //下半身攻撃
-            if (Input.GetKeyDown(KeyCode.K))
+            playerRigidBody2D.AddForce(transform.up * playerJumpPower);
+            MultiAudio.ins.PlaySEByName("SE_hero_action_jump");
+            isJump = true;
+            jumpCount++;
+            if (playerRigidBody2D.velocity.y > playerJumpPower / playerRigidBody2D.mass)
             {
-                #region 山品変更
-                playerMoveAnimation.KickStart();
-                #endregion
-                switch (PlayerParameter.Instance.LowerData.lowerAttack)
-                {
-                    case LowerAttack.NORMAL:
-                        MultiAudio.ins.PlaySEByName("SE_hero_attack_lower");
-
-                        break;
-
-                    case LowerAttack.POLICE:
-                        MultiAudio.ins.PlaySEByName("SE_policeofficer_attack_lower");
-                        break;
-
-                    case LowerAttack.NURSE:
-                        MultiAudio.ins.PlaySEByName("SE_nurse_attack_lower");
-                        break;
-                }
-                for (int i = 0; i < enemyObject.Count; i++)
-                {
-                    //仮引数
-                    LowerBodyAttack(i, enemyObject[i].transform.position, PlayerParameter.Instance.LowerData.AttackArea, PlayerParameter.Instance.LowerData.iPartAttack);
-                }
+                playerRigidBody2D.velocity = new Vector2(playerRigidBody2D.velocity.x, playerJumpPower / playerRigidBody2D.mass);
             }
         }
-
-
     }
+}
+private void UpdateTimers()
+{
+    playerMoveAnimation.timeWalk -= Time.deltaTime;
+    playerMoveAnimation.timeAttack -= Time.deltaTime;
+}
+//ゲームメインのエクスキュート
+void MainExecution()
+{
+    //現在のポジションを取得
+    Vector3 vPosition = transform.position;
 
-    //アニメーションが持っていた関数を移動（アニメーションそのものには関係ないため）
-    /// <summary>
-    /// 上半身のイメージ
-    /// </summary>
-    /// <param name="upperBody">画像データ集合体</param>
-    public void ChangeUpperBody(BodyPartsData upperBody)
+    //カメラとの距離の絶対値が一定以下ならプレイヤーが動く　画面外に出ないための処置
+    //移動
+    Vector3 vPosFromCame = vPosition - mainCamera.transform.position; //カメラ基準のプレイヤーの位置
+
+    if (!playerMoveAnimation.SetAttack())
     {
-        characterSprites.body.sprite = upperBody.spBody;
-        characterSprites.armRight.sprite = upperBody.spRightArm;
-        characterSprites.armLeft.sprite = upperBody.spLeftArm;
-        characterSprites.handRight.sprite = upperBody.spRightHand;
-        characterSprites.handLeft.sprite = upperBody.spLeftHand;
-    }
-    //アニメーションが持っていた関数を移動（アニメーションそのものには関係ないため）
-
-    /// <summary>
-    /// 下半身のイメージ
-    /// </summary>
-    /// <param name="underBody">画像データ集合体</param>
-    public void ChangeUnderBody(BodyPartsData underBody)
-    {
-        characterSprites.waist.sprite = underBody.spWaist;
-        characterSprites.footRight.sprite = underBody.spRightFoot;
-        characterSprites.footLeft.sprite = underBody.spLeftFoot;
-        characterSprites.legRight.sprite = underBody.spRightLeg;
-        characterSprites.legLeft.sprite = underBody.spLeftLeg;
-    }
-
-
-
-    //上半身攻撃
-    public void UpperBodyAttack(int EnemyNum, Vector3 vTargetPos, float fReach, int iDamage)
-    {
-        float fAttackReach = Vector3.Distance(vTargetPos, transform.position);
-        if (fAttackReach >= fReach)
+        //左移動
+        if (Input.GetKey(KeyCode.A))
         {
-            return;
-        }
-        IDamageable damageable = enemyObject[EnemyNum].GetComponent<IDamageable>();
-        damageable?.TakeDamage(iDamage, 0);
-
-    }
-    //下半身攻撃
-    public void LowerBodyAttack(int EnemyNum, Vector3 vTargetPos, float fReach, int iDamage)
-    {
-        float fAttackReach = Vector3.Distance(vTargetPos, transform.position);
-        if (fAttackReach >= fReach)
-        {
-            return;
-        }
-        IDamageable damageable = enemyObject[EnemyNum].GetComponent<IDamageable>();
-        damageable?.TakeDamage(iDamage, 1);
-    }
-
-
-    public void AddListItem(GameObject obj) => enemyObject.Add(obj);
-    public void RemoveListItem(GameObject obj) => enemyObject.Remove(obj);
-
-    //床判定
-    private void OnCollisionEnter2D(Collision2D other)
-    {
-        if (other.gameObject.CompareTag("Floor") || other.gameObject.CompareTag("Car"))
-        {
-            isJump = false;
-            jumpCount = 0;
-        }
-    }
-
-
-    //敵の弾都の当たり判定
-    private void OnTriggerEnter2D(Collider2D playerCollision)
-    {
-        if (playerCollision.gameObject.CompareTag("EnemyShoot"))
-        {
-            if (0 > transform.position.y - playerCollision.transform.position.y)
+            if (vPosFromCame.x > -mainCameraWidth / 2)
             {
-                PlayerParameter.Instance.UpperHP -= 1;
+                vPosition.x -= Time.deltaTime * playerSpeed;
+
             }
-            else
+            playerMoveAnimation.HandleWalk(180);
+        }
+        //右移動
+        if (Input.GetKey(KeyCode.D))
+        {
+            if (mainCameraWidth / 2 > vPosFromCame.x)
             {
-                PlayerParameter.Instance.LowerHP -= 1;
+                vPosition.x += Time.deltaTime * playerSpeed;
             }
+            playerMoveAnimation.HandleWalk(0);
+
         }
 
+        //ジャンプ
+
+        if (Input.GetKey(KeyCode.W) && jumpCount < 1)
+        {
+            GetComponent<Rigidbody2D>().AddForce(transform.up * playerJumpPower);
+            MultiAudio.ins.PlaySEByName("SE_hero_action_jump");
+            isJump = true;
+            jumpCount++;
+        }
+
+        //楽に次のシーン行きたいならこの下のコードをコメントアウト解除　確認後コメントアウトしておいて
+
+        //if (Input.GetKeyDown(KeyCode.Escape))
+        //{
+        //    SceneTransitionManager.instance.NextSceneButton(SceneTransitionManager.instance.sceneInformation.GetCurrentScene() + 1); 
+        //}
+        //ここまで
+        //楽にボス戦行きたいなら以下のコードをコメント解除
+        //if (Input.GetKeyDown(KeyCode.S))
+        //{
+        //    vPosition = new Vector2(190.0f, -1.536416f);
+        //}
+        //ここまで
+
     }
+
+    //体が回転しないようにするのオイラーを０で設定すればできる
+    //自分のtransformを取得
+
+    transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+    transform.position = vPosition;
+
+
+
+
+    #region 山品変更
+    //アニメーションをここで呼ぶため、追記
+    //攻撃関連
+    if (!playerMoveAnimation.SetAttack() && playerMoveAnimation.timeAttack < 0)
+    {
+        //上半身攻撃
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+
+            playerMoveAnimation.PantieStart();
+            #endregion
+            switch (PlayerParameter.Instance.UpperData.upperAttack)
+            {
+                case UpperAttack.NORMAL:
+
+                    MultiAudio.ins.PlaySEByName("SE_hero_attack_upper");
+
+                    break;
+
+                case UpperAttack.POLICE:
+                    Vector2 ShootMoveBector = new Vector2(0, 0);
+                    //子のplayerRCのローテーションYを持ってくる
+                    // y = 0のときは右向き、0 y = 180のときは左向き
+                    Debug.Log(transform.GetChild(0).transform.eulerAngles.y);
+                    if (transform.GetChild(0).transform.eulerAngles.y == 180)
+                    {
+                        ShootMoveBector.x = -1;
+                    }
+                    else
+                    {
+                        ShootMoveBector.x = 1;
+                    }
+
+                    Debug.Log(ShootMoveBector);
+                    Debug.Log("shootFlagは" + isShot);
+
+                    //isShotがtrueなら銃を発射する
+                    if (isShot == true)
+                    {
+                        Debug.Log("弾発射");
+                        Gun.Shoot(ShootMoveBector, transform);
+
+                        MultiAudio.ins.PlaySEByName("SE_policeofficer_attack_upper");
+
+                        //bShootFlag = false;
+                    }
+                    break;
+
+                case UpperAttack.NURSE:
+                    MultiAudio.ins.PlaySEByName("SE_nurse_attack_upper");
+                    break;
+            }
+
+            if (PlayerParameter.Instance.UpperData.sPartsName == "ボスの上半身")
+            {
+                MultiAudio.ins.PlaySEByName("SE_lastboss_attack_upper");
+            }
+
+            for (int i = 0; i < enemyObject.Count; i++)
+            {
+                //Debug.Log(liObj[i].gameObject.transform.position);
+                //Debug.Log(playerParameter.UpperData.AttackArea);
+                //仮引数
+                UpperBodyAttack(i, enemyObject[i].transform.position, PlayerParameter.Instance.UpperData.AttackArea, PlayerParameter.Instance.UpperData.iPartAttack);
+            }
+        }
+        //下半身攻撃
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            #region 山品変更
+            playerMoveAnimation.KickStart();
+            #endregion
+            switch (PlayerParameter.Instance.LowerData.lowerAttack)
+            {
+                case LowerAttack.NORMAL:
+                    MultiAudio.ins.PlaySEByName("SE_hero_attack_lower");
+
+                    break;
+
+                case LowerAttack.POLICE:
+                    MultiAudio.ins.PlaySEByName("SE_policeofficer_attack_lower");
+                    break;
+
+                case LowerAttack.NURSE:
+                    MultiAudio.ins.PlaySEByName("SE_nurse_attack_lower");
+                    break;
+            }
+            for (int i = 0; i < enemyObject.Count; i++)
+            {
+                //仮引数
+                LowerBodyAttack(i, enemyObject[i].transform.position, PlayerParameter.Instance.LowerData.AttackArea, PlayerParameter.Instance.LowerData.iPartAttack);
+            }
+        }
+    }
+
+
+}
+
+//アニメーションが持っていた関数を移動（アニメーションそのものには関係ないため）
+/// <summary>
+/// 上半身のイメージ
+/// </summary>
+/// <param name="upperBody">画像データ集合体</param>
+public void ChangeUpperBody(BodyPartsData upperBody)
+{
+    characterSprites.body.sprite = upperBody.spBody;
+    characterSprites.armRight.sprite = upperBody.spRightArm;
+    characterSprites.armLeft.sprite = upperBody.spLeftArm;
+    characterSprites.handRight.sprite = upperBody.spRightHand;
+    characterSprites.handLeft.sprite = upperBody.spLeftHand;
+}
+//アニメーションが持っていた関数を移動（アニメーションそのものには関係ないため）
+
+/// <summary>
+/// 下半身のイメージ
+/// </summary>
+/// <param name="underBody">画像データ集合体</param>
+public void ChangeUnderBody(BodyPartsData underBody)
+{
+    characterSprites.waist.sprite = underBody.spWaist;
+    characterSprites.footRight.sprite = underBody.spRightFoot;
+    characterSprites.footLeft.sprite = underBody.spLeftFoot;
+    characterSprites.legRight.sprite = underBody.spRightLeg;
+    characterSprites.legLeft.sprite = underBody.spLeftLeg;
+}
+
+
+
+//上半身攻撃
+public void UpperBodyAttack(int EnemyNum, Vector3 vTargetPos, float fReach, int iDamage)
+{
+    float fAttackReach = Vector3.Distance(vTargetPos, transform.position);
+    if (fAttackReach >= fReach)
+    {
+        return;
+    }
+    IDamageable damageable = enemyObject[EnemyNum].GetComponent<IDamageable>();
+    damageable?.TakeDamage(iDamage, 0);
+
+}
+//下半身攻撃
+public void LowerBodyAttack(int EnemyNum, Vector3 vTargetPos, float fReach, int iDamage)
+{
+    float fAttackReach = Vector3.Distance(vTargetPos, transform.position);
+    if (fAttackReach >= fReach)
+    {
+        return;
+    }
+    IDamageable damageable = enemyObject[EnemyNum].GetComponent<IDamageable>();
+    damageable?.TakeDamage(iDamage, 1);
+}
+
+
+public void AddListItem(GameObject obj) => enemyObject.Add(obj);
+public void RemoveListItem(GameObject obj) => enemyObject.Remove(obj);
+
+//床判定
+private void OnCollisionEnter2D(Collision2D other)
+{
+    if (other.gameObject.CompareTag("Floor") || other.gameObject.CompareTag("Car"))
+    {
+        isJump = false;
+        jumpCount = 0;
+    }
+}
+
+
+//敵の弾都の当たり判定
+private void OnTriggerEnter2D(Collider2D playerCollision)
+{
+    if (playerCollision.gameObject.CompareTag("EnemyShoot"))
+    {
+        if (0 > transform.position.y - playerCollision.transform.position.y)
+        {
+            PlayerParameter.Instance.UpperHP -= 1;
+        }
+        else
+        {
+            PlayerParameter.Instance.LowerHP -= 1;
+        }
+    }
+
+}
 
 }
