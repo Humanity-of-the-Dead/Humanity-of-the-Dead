4dda2c987a5eed0b42f0da9f1893d67681a986b2
maho0628 <maho06282001@gmail.com>
2025-02-01 15:56:38 +0900

    #29修正？？？

--- Assets/Sakamoto/Scripts/CameraScript.cs
+++ Assets/Sakamoto/Scripts/CameraScript.cs
@@ -7,129 +7,91 @@ using UnityEngine.SceneManagement;
 enum STATE
 {
     NONE,
-    NOMAL,//ノーマルステージ
-    BOSS,//ボスステージ
+    NOMAL, // ノーマルステージ
+    BOSS   // ボスステージ
 }
-
 
 public class CameraScript : MonoBehaviour
 {
     [SerializeField] private GameObject goTarget;
     [SerializeField] private float fMoveStart;
     [SerializeField] private float fMoveLimit;
+    [SerializeField] private float smoothSpeed = 5.0f; // カメラの追従速度
 
-    ////カメラから見たターゲットの位置
-    //Vector2 fTrgPosFromCamera;
-
-    //ゲームステート
     private STATE eState = STATE.NONE;
 
-    //bool fMoveRight;
-    //bool fMoveLeft;
-    // Start is called before the first frame update
     void Start()
     {
         eState = STATE.NOMAL;
         goTarget = GameObject.Find("Player Variant");
     }
 
-    // Update is called once per frame
     void Update()
+    {
+        // シーン名を取得してボスステージかどうか判定
+        string sceneName = SceneManager.GetActiveScene().name;
+        var bossScenes = new[]
+        {
+            SceneTransitionManager.instance.sceneInformation.GetSceneName(SceneInformation.SCENE.StageOne_BOSS),
+            SceneTransitionManager.instance.sceneInformation.GetSceneName(SceneInformation.SCENE.StageTwo_BOSS),
+            SceneTransitionManager.instance.sceneInformation.GetSceneName(SceneInformation.SCENE.StageThree_BOSS),
+            SceneTransitionManager.instance.sceneInformation.GetSceneName(SceneInformation.SCENE.StageFour),
+            SceneTransitionManager.instance.sceneInformation.GetSceneName(SceneInformation.SCENE.StageFive)
+        };
+
+        if (bossScenes.Contains(sceneName))
+        {
+            eState = STATE.BOSS;
+        }
+    }
+
+    void LateUpdate()
     {
         switch (eState)
         {
             case STATE.NOMAL:
-                //プレイヤーが画面中央に来たら追従する
-                Vector3 vCamPos = transform.position;
-                if (goTarget.transform.position.x > fMoveStart)
-                {
-                    vCamPos.x = goTarget.transform.position.x;
-                    transform.position = vCamPos;
-                }
-                if (transform.position.x > fMoveLimit)
-                {
-                    vCamPos.x = fMoveLimit;
-                    transform.position = vCamPos;
-                }
-                vCamPos.y = goTarget.transform.position.y;
-                string sceneName = SceneManager.GetActiveScene().name;
-                var bossScenes = new[]
-                {
-                  SceneTransitionManager.instance.sceneInformation.GetSceneName(SceneInformation.SCENE.StageOne_BOSS),
-                  SceneTransitionManager.instance.sceneInformation.GetSceneName(SceneInformation.SCENE.StageTwo_BOSS),
-                  SceneTransitionManager.instance.sceneInformation.GetSceneName(SceneInformation.SCENE.StageThree_BOSS),
-                  SceneTransitionManager.instance.sceneInformation.GetSceneName(SceneInformation.SCENE.StageFour),
-                  SceneTransitionManager.instance.sceneInformation.GetSceneName(SceneInformation.SCENE.StageFive)
-                };
-
-                if (bossScenes.Contains(sceneName))
-                {
-                    eState = STATE.BOSS;
-                }
-
-                if (vCamPos.y < 0)
-                {
-                    vCamPos.y = 0;
-                }
-                transform.position = vCamPos;
+                FollowPlayer();
                 break;
+
             case STATE.BOSS:
-                Vector3 bossCamPos = transform.position;
-
-                // カメラのX座標を固定
-                bossCamPos.x = fMoveLimit;
-
-                // プレイヤーのジャンプを追従しつつ制約を追加
-                float targetY = goTarget.transform.position.y;
-                bossCamPos.y = Mathf.Clamp(targetY, 0, 2);
-
-                transform.position = bossCamPos;
-
-                //カメラ追従なし
+                FollowBossMode();
                 break;
         }
     }
+
+    // プレイヤーをスムーズに追従する処理
+    void FollowPlayer()
+    {
+        Vector3 targetPos = transform.position;
+
+        // X方向の追従
+        if (goTarget.transform.position.x > fMoveStart)
+        {
+            targetPos.x = Mathf.Lerp(transform.position.x, goTarget.transform.position.x, Time.deltaTime * smoothSpeed);
+        }
+
+        // Xの上限を適用
+        targetPos.x = Mathf.Min(targetPos.x, fMoveLimit);
+
+        // Y方向の追従（最小0に制限）
+        targetPos.y = Mathf.Lerp(transform.position.y, goTarget.transform.position.y, Time.deltaTime * smoothSpeed);
+        targetPos.y = Mathf.Max(targetPos.y, 0);
+
+        transform.position = targetPos;
+    }
+
+    // ボス戦時のカメラ制御
+    void FollowBossMode()
+    {
+        Vector3 targetPos = transform.position;
+
+        // X座標を固定
+        targetPos.x = fMoveLimit;
+
+        // Y座標を制限付きで追従
+        float targetY = Mathf.Lerp(transform.position.y, goTarget.transform.position.y, Time.deltaTime * smoothSpeed);
+        targetPos.y = Mathf.Clamp(targetY, 0, 2);
+
+        transform.position = targetPos;
+    }
 }
-
-//旧アップデートの中身
-////ターゲットの相対位置の取得
-//fTrgPosFromCamera = goTarget.transform.position - this.transform.position;
-////ターゲットのx位置が3以上ならカメラを右に移動させる
-//if(fTrgPosFromCamera.x > 3 && fMoveRight == false)
-//{
-//    fMoveRight = true;
-//}
-////ターゲットのx位置が-3以下ならカメラを左手に移動させる
-//if (fTrgPosFromCamera.x < -3 && fMoveLeft == false)
-//{
-//    fMoveLeft = true;
-//}
-
-//Debug.Log("右" + fMoveRight);
-//Debug.Log("左" + fMoveLeft);
-//if(fMoveRight == true)
-//{
-//    if (this.transform.position.x < fMoveLimit)
-//    {
-//        Vector3 pos = this.transform.position;
-//        pos.x += 0.2f;
-//        this.transform.position = pos;
-//        if(fTrgPosFromCamera.x < 0)
-//        {
-//            fMoveRight = false;
-//        }
-//    }
-//}
-//if(fMoveLeft == true)
-//{
-//    if (this.transform.position.x > 0)
-//    {
-//        Vector3 pos = this.transform.position;
-//        pos.x -= 0.2f;
-//        this.transform.position = pos;
-//        if(fTrgPosFromCamera.x > 0)
-//        {
-//            fMoveLeft = false;
-//        }
-//    }
-//}
