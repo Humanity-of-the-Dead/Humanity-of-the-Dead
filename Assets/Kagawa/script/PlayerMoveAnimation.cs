f0b4a61f0f79b82d495aa8ff3af790a69e5ff984
maho0628 <maho06282001@gmail.com>
2025-02-04 02:11:32 +0900

    プレイヤーからのエフェクト修正

--- Assets/Kagawa/script/PlayerMoveAnimation.cs
+++ Assets/Kagawa/script/PlayerMoveAnimation.cs
@@ -137,37 +137,33 @@ public class PlayerMoveAnimation : MonoBehaviour
         }
     }
 
-    public void ShowHitEffects(int body)
+    public void ShowHitEffects(int body, Vector3 enemyVector3)
     {
-        PlayerControl playerControl = GetComponent<PlayerControl>();
-        //このオブジェクトの座標
-        for (int i = 0; i < playerControl.enemyObject.Count; i++)
+
+        //上半身の場合
+        if (body == 0)
         {
-            Vector3 enemyVector3 = new Vector3(playerControl.enemyObject[i].transform.position.x, playerControl.enemyObject[i].transform.position.y);
-            //上半身の場合
-            if (body == 0)
-            {
-                //オブジェクトを出すローカル座標
-                Vector3 effectVec2Upper = new Vector3(
-                    Random.Range(upperEffectXMin, upperEffectXMax),
-                    Random.Range(upperEffectYMin, upperEffectYMax));
+            //オブジェクトを出すローカル座標
+            Vector3 effectVec2Upper = new Vector3(
+                Random.Range(upperEffectXMin, upperEffectXMax),
+                Random.Range(upperEffectYMin, upperEffectYMax));
 
-                //オブジェクトを出す
-                Instantiate(hitGameObject, effectVec2Upper + enemyVector3, Quaternion.identity);
-                // Debug.Log("effectVec2+thisVec2="+effectVec2+tihsVec2)
-                // Debug.Log("hit effect");
-            }
-            if (body == 1)
-            {
-                //オブジェクトを出すローカル座標
-                Vector3 effectVec3Lower = new Vector2(
-                    Random.Range(lowerEffectXMin, lowerEffectXMax),
-                    Random.Range(lowerEffectYMin, lowerEffectYMax));
-
-                Instantiate(hitGameObject, effectVec3Lower + enemyVector3, Quaternion.identity);
-            }
-
+            //オブジェクトを出す
+            Instantiate(hitGameObject, effectVec2Upper + enemyVector3, Quaternion.identity);
+            // Debug.Log("effectVec2+thisVec2="+effectVec2+tihsVec2)
+            // Debug.Log("hit effect");
         }
+        if (body == 1)
+        {
+            //オブジェクトを出すローカル座標
+            Vector3 effectVec3Lower = new Vector3(
+                Random.Range(lowerEffectXMin, lowerEffectXMax),
+                Random.Range(lowerEffectYMin, lowerEffectYMax));
+
+            Instantiate(hitGameObject, effectVec3Lower + enemyVector3, Quaternion.identity);
+        }
+
+
 
 
 
