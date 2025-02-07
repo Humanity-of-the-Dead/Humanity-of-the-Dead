7650cc6de133203dc25c336efca4f7cf3227967a
maho <149309001+maho0628@users.noreply.github.com>
2025-02-07 20:16:14 +0900

    ビルドのusing削除

--- Assets/Tanaka/scripts/TextDisplay.cs
+++ Assets/Tanaka/scripts/TextDisplay.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
 using UnityEngine.SceneManagement;
-using static UnityEditor.Experimental.GraphView.GraphView;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
