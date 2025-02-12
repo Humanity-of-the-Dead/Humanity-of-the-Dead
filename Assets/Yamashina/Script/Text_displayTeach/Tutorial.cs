961a9f0e0a3104eed2bf92d9d2c81a00ba509c43
maho <149309001+maho0628@users.noreply.github.com>
2025-02-13 00:03:13 +0900

    チュートリアルステート追加

--- Assets/Yamashina/Script/Text_displayTeach/Tutorial.cs
+++ Assets/Yamashina/Script/Text_displayTeach/Tutorial.cs
@@ -5,13 +5,36 @@ public class Tutorial : TextDisplay
     private Tutorial_spown tutorial_Spawn;
     public enum Tutorial_State
     {
-       
+        PlayerMove,
+        PlayerGauge,
+        PlayerAttack,
+        PlayerTransplant,
+        EnemyDrop,
+        PlayerComfort,
+        Option
     }
-
+    static Tutorial_State enGameState;
+    static Tutorial_State previousGameState; // 前回のゲームステートを保存
     protected override void Start()
     {
         base.Start();
         tutorial_Spawn = FindAnyObjectByType<Tutorial_spown>();
+    }
+    public static void ChangeState(Tutorial_State newState)
+    {
+        previousGameState = enGameState; // 現在のステートを前回のステートとして保存
+
+        enGameState = newState;
+        Debug.Log(newState);
+    }
+    // ステートが変わったかを確認する関数
+    public static bool HasStateChanged()
+    {
+        return enGameState != previousGameState;
+    }
+    public static Tutorial_State GetState()
+    {
+        return enGameState;
     }
     protected override void Update()
     {
@@ -29,7 +52,7 @@ public class Tutorial : TextDisplay
                 {
                     GameMgr.ChangeState(GameState.Tutorial);
                     tutorial_Spawn.SpawnTutorial();
-
+                    ChangeState(Tutorial_State.PlayerMove);
 
 
                 }
@@ -41,10 +64,10 @@ public class Tutorial : TextDisplay
                 }
                 break;
             case GameState.Hint:
-                base.Update(); break;   
+                base.Update(); break;
 
             case GameState.Clear:
-                base.Update();  
+                base.Update();
                 break;
 
 
@@ -74,5 +97,11 @@ public class Tutorial : TextDisplay
     {
         base.UpdateHintText();
     }
-
+    private void OnGUI()
+    {
+        GUI.skin.label.fontSize = 30;  // 例えば30に設定
+        GUI.skin.label.normal.textColor = Color.black;
+        GUI.skin.label.fontStyle = FontStyle.Bold;  
+        GUI.Label(new Rect(10.0f,400.0f, Screen.width, Screen.height), enGameState.ToString());
+    }
 }
