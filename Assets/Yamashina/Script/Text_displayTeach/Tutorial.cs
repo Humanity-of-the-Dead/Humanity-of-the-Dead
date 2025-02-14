9cb1e1ec8996bde52a356113f881fce5d4db2e4f
maho <149309001+maho0628@users.noreply.github.com>
2025-02-14 16:56:12 +0900

    クリア演出強化

--- Assets/Yamashina/Script/Text_displayTeach/Tutorial.cs
+++ Assets/Yamashina/Script/Text_displayTeach/Tutorial.cs
@@ -23,7 +23,7 @@ public class Tutorial : TextDisplay
         int nextIndex = (int)enGameState + 1; // 次のインデックス
         if (nextIndex < System.Enum.GetValues(typeof(Tutorial_State)).Length)
         {
-            ChangeState( (Tutorial_State)nextIndex);
+            ChangeState((Tutorial_State)nextIndex);
         }
         else
         {
@@ -45,7 +45,7 @@ public class Tutorial : TextDisplay
         previousGameState = enGameState; // 現在のステートを前回のステートとして保存
 
         enGameState = newState;
-        Debug.Log("ChangeState"+newState);
+        Debug.Log("ChangeState" + newState);
     }
     // ステートが変わったかを確認する関数
     public static bool HasStateChanged()
@@ -58,7 +58,7 @@ public class Tutorial : TextDisplay
     }
     private void ChangeStateToDoNotMoveIfNeeded()
     {
-        if( enGameState == Tutorial_State.PlayerGauge && Player.transform.position.x> POSITION_DONOT_MOVE)
+        if (enGameState == Tutorial_State.PlayerGauge && Player.transform.position.x > POSITION_DONOT_MOVE)
         {
             ChangeState(Tutorial_State.PlayerDoNotMove);
         }
@@ -72,9 +72,9 @@ public class Tutorial : TextDisplay
             case GameState.Main:
                 base.Update();
                 ChangeStateToDoNotMoveIfNeeded();
-                if (Tutorial.GetState() == Tutorial_State.Option)
+                if (GetState() == Tutorial_State.Option)
                 {
-                    GameMgr.ChangeState(GameState.Clear);
+                    GameMgr.ChangeState(GameState.AfterBoss);
                 }
                 break;
             case GameState.ShowText:
@@ -83,7 +83,7 @@ public class Tutorial : TextDisplay
                 {
                     GameMgr.ChangeState(GameState.Tutorial);
                     tutorial_Spawn.SpawnTutorial();
-                    Debug.Log(GameMgr.GetState().ToString());   
+                    Debug.Log(GameMgr.GetState().ToString());
 
                 }
                 break;
@@ -94,6 +94,8 @@ public class Tutorial : TextDisplay
                 }
                 break;
             case GameState.Hint:
+                base.Update(); break;
+            case GameState.AfterBoss:
                 base.Update(); break;
 
             case GameState.Clear:
@@ -140,7 +142,7 @@ public class Tutorial : TextDisplay
                 UpdateText();
                 //テキスト表示域を表示域
                 TextArea.SetActive(true);
-                if(i != 0)
+                if (i != 0)
                 {
                     NextState();
                 }
@@ -153,7 +155,7 @@ public class Tutorial : TextDisplay
     {
         GUI.skin.label.fontSize = 30;  // 例えば30に設定
         GUI.skin.label.normal.textColor = Color.black;
-        GUI.skin.label.fontStyle = FontStyle.Bold;  
-        GUI.Label(new Rect(10.0f,400.0f, Screen.width, Screen.height), enGameState.ToString());
+        GUI.skin.label.fontStyle = FontStyle.Bold;
+        GUI.Label(new Rect(10.0f, 400.0f, Screen.width, Screen.height), enGameState.ToString());
     }
 }
