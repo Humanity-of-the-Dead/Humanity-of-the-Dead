eaa064f504cbbdae01e5bad1b893c9ee5a97534e
maho <149309001+maho0628@users.noreply.github.com>
2025-02-17 23:55:19 +0900

    炎の床修正

--- Assets/Sakamoto/Scripts/GameMgr.cs
+++ Assets/Sakamoto/Scripts/GameMgr.cs
@@ -111,11 +111,11 @@ public class GameMgr : MonoBehaviour
 
     }
 
-    private void OnGUI()
-    {
-        GUI.skin.label.fontSize = 30;  // ó·Ç¶ÇŒ30Ç…ê›íË
+    //private void OnGUI()
+    //{
+    //    GUI.skin.label.fontSize = 30;  // ó·Ç¶ÇŒ30Ç…ê›íË
 
-        GUI.Label(new Rect(10.0f, 10.0f, Screen.width, Screen.height), enGameState.ToString());
-    }
+    //    GUI.Label(new Rect(10.0f, 10.0f, Screen.width, Screen.height), enGameState.ToString());
+    //}
 }
 
